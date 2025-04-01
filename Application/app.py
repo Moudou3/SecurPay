6006d25f5f282c6bb04588529bea8b35787bc4e0
Wiam24 <w.belmehdi@esisa.ac.ma>
2025-04-01 17:04:47 +0200

    page de connexion

--- Application/app.py
+++ Application/app.py
@@ -47,6 +47,10 @@ def home2():
 def about():
     return render_template('about.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
 @app.route('/logout')
 def logout():
     return render_template('home.html')
